1a286b95358a2de4f77a2b31847f5f34070fca12
davettt <mail@davidtiong.com.au>
2015-02-10 22:17:39 +1100

    modified function

--- functions.php
+++ functions.php
@@ -11,12 +11,14 @@ function spectragram_shortcode($atts, $content = null){
     $spectragramdata = shortcode_atts(array(
         'query' => '',
         'max' => '9',
-        'size' => 'small'
+        'size' => 'small',
+        'class' => 'instagrid'
     ),$atts);
     wp_enqueue_script('spectragram');
     wp_enqueue_script('spectragramscript');
     wp_localize_script('spectragramscript','instag',$spectragramdata);
-    $instagram_spectragram = '<ul class="instagrid"></ul>';
+    $spectra_class = $spectragramdata['class'];
+    $instagram_spectragram = '<ul class="'.$spectra_class.'"></ul>';
     return $instagram_spectragram;
 }
 add_shortcode('instagrid','spectragram_shortcode');
