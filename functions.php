bbbdd8323d47c6bd56e138fe6a9f7a627ccb9c7b
davettt <mail@davidtiong.com.au>
2015-02-10 21:18:16 +1100

    updated functions demo

--- functions.php
+++ functions.php
@@ -2,16 +2,16 @@
 /*demo for functions childtheme file*/
 
 function add_spectragram_files() {
-    wp_register_style('spectragram',get_stylesheet_directory_uri().'/js/spectragram/spectragram.min.js',array('jquery'),'',true);
-    wp_register_style('spectragramscript',get_stylesheet_directory_uri().'/js/spectragram/spectragramscript',array('spectragram'),'',true);
+    wp_register_script('spectragram',get_stylesheet_directory_uri().'/js/spectragram/spectragram.min.js',array('jquery'),'',true);
+    wp_register_script('spectragramscript',get_stylesheet_directory_uri().'/js/spectragram/spectragramscript.js',array('spectragram'),'',true);
 }
 add_action('wp_enqueue_scripts','add_spectragram_files');
 
 function spectragram_shortcode($atts, $content = null){
     $spectragramdata = shortcode_atts(array(
         'query' => '',
-        'max' => '10',
-        'size' => 'medium'
+        'max' => '9',
+        'size' => 'small'
     ),$atts);
     wp_enqueue_script('spectragram');
     wp_enqueue_script('spectragramscript');
