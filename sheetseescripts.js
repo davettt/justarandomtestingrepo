776e933a263c5e6a42cf14428cf9cca8eebd9977
davettt <mail@davidtiong.com.au>
2015-02-12 19:37:59 +1100

    changes

--- sheetseescripts.js
+++ sheetseescripts.js
@@ -3,14 +3,14 @@
 jQuery(document).ready(function($) {
         $("span.sheetseepie").each(function(index) {
             sheetseeurl = $(this).text();
-             sheetseeid = $(this).parent("div").attr("id");
-             sheetseelabel = $(this).data("label");
-             sheetseeunit = $(this).data("unit");
-             sheetseemargin = $(this).data("margin");
-             sheetseewidth = $(this).data("width");
-             sheetseeheight = $(this).data("height");
-             sheetseehoriz = $(this).data("horiz");
-             sheetseehi = $(this).data("hi");
+            var sheetseeid = $(this).parent("div").attr("id");
+            var sheetseelabel = $(this).data("label");
+            var sheetseeunit = $(this).data("unit");
+            var sheetseemargin = $(this).data("margin");
+            var sheetseewidth = $(this).data("width");
+            var sheetseeheight = $(this).data("height");
+            var sheetseehoriz = $(this).data("horiz");
+            var sheetseehi = $(this).data("hi");
             console.log(sheetseeurl);
             console.log(sheetseeid);
             console.log(sheetseelabel);
@@ -23,21 +23,21 @@ jQuery(document).ready(function($) {
             $("span.sheetseepie").empty();
             Tabletop.init({ 
                 key: sheetseeurl,
-                callback: showInfo(),
+                callback: showInfo,
                 simpleSheet: true 
             });
     });
 function showInfo(data) {
     var url= sheetseeurl;
     var pieOptions = {
-    labels: sheetseelabel, 
-    units: sheetseeunit, 
-    m: sheetseemargin, 
-    w: sheetseewidth, 
-    h: sheetseeheight, 
-    div: sheetseeid, 
-    xaxis: sheetseehoriz,
-    hiColor: sheetseehi
+    labels: $(this).data("label"), 
+    units: $(this).data("unit"), 
+    m: $(this).data("margin"), 
+    w: $(this).data("width"), 
+    h: $(this).data("height"), 
+    div: $(this).parent("div").attr("id"), 
+    xaxis: $(this).data("horiz"),
+    hiColor: $(this).data("hi")
     };
             console.log(url);
     Sheetsee.d3PieChart(data,pieOptions);
