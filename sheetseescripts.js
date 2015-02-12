b08ae32cc08b970d0a1da777539bac7a11fe4bb4
davettt <mail@davidtiong.com.au>
2015-02-12 15:23:32 +1100

    new files

---
+++ sheetseescripts.js
@@ -0,0 +1,35 @@
+/* script for Sheetsee shortcode */
+
+jQuery(document).ready(function($) {
+    if ($("span.sheetseepie").exists()) {
+        $("span.sheetseepie").each(function(index) {
+            var sheetseeurl = $(this).text();
+            var sheetseeid = $(this).parent("div").attr("id");
+            var sheetseelabel = $(this).data("label");
+            var sheetseeunit = $(this).data("unit");
+            var sheetseewidth = $(this).data("width");
+            var sheetseeheight = $(this).data("height");
+            console.log(sheetseeurl);
+            console.log(sheetseeid);
+            console.log(sheetseelabel);
+            console.log(sheetseeunit);
+            console.log(sheetseewidth);
+            console.log(sheetseeheight);
+        });
+   /* var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1gu2f0fKjrHa2GSM_hdl4FHaJW1bKEoZljI25NDfH_QU/pubhtml';
+    Tabletop.init({ 
+        key: public_spreadsheet_url,
+        callback: showInfo,
+        simpleSheet: true 
+    });
+function showInfo(data) {
+    var pieOptions = {
+    labels: "Content Management", 
+    units: "Usage", 
+    m: [80, 80, 80, 80], 
+    w: 600, h: 400, 
+    div: "#hello", 
+    hiColor: "#14ECC8"
+    };
+    Sheetsee.d3PieChart(data,pieOptions);
+}*/}} );
