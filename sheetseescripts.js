2fb06a0547d2180bbb83f593d20ed6449a262fa6
davettt <mail@davidtiong.com.au>
2015-02-13 16:27:10 +1100

    line

--- sheetseescripts.js
+++ sheetseescripts.js
@@ -115,4 +115,61 @@ function showInfoBar(data) {
     Sheetsee.d3BarChart(data,barOptions);
     $("span.sheetseebar").empty();
 }    });
+$("span.sheetseeline").each(function(index) {
+            var sheetseeurl = $(this).text();
+            var sheetseeid = $(this).parent("div").attr("id");
+            var sheetseelabel = $(this).data("label");
+            var sheetseeunit = $(this).data("unit");
+            var sheetseemargin = $(this).data("margin");
+            var sheetseewidth = $(this).data("width");
+            var sheetseeheight = $(this).data("height");
+            var sheetseehoriz = $(this).data("horiz");
+            var sheetseehi = $(this).data("hi");
+            console.log(sheetseeurl);
+            console.log(sheetseeid);
+            console.log(sheetseelabel);
+            console.log(sheetseeunit);
+            console.log(sheetseemargin);
+            console.log(sheetseewidth);
+            console.log(sheetseeheight);
+            console.log(sheetseehoriz);
+            console.log(sheetseehi);
+            //$("span.sheetseepie").empty();
+            Tabletop.init({ 
+                key: sheetseeurl,
+                callback: showInfoLine,
+                simpleSheet: true 
+            });
+function showInfoLine(data) {
+    /*var sheetseeurl = $("span.sheetseepie").text();
+    var sheetseeid = $("span.sheetseepie").parent("div").attr("id");
+    var sheetseelabel = $("span.sheetseepie").data("label");
+    var sheetseeunit = $("span.sheetseepie").data("unit");
+    var sheetseemargin = $("span.sheetseepie").data("margin");
+    var sheetseewidth = $("span.sheetseepie").data("width");
+    var sheetseeheight = $("span.sheetseepie").data("height");
+    var sheetseehoriz = $("span.sheetseepie").data("horiz");
+    var sheetseehi = $("span.sheetseepie").data("hi");
+                console.log(sheetseeurl);*/
+            console.log(sheetseeid);/*
+            console.log(sheetseelabel);
+            console.log(sheetseeunit);
+            console.log(sheetseemargin);
+            console.log(sheetseewidth);
+            console.log(sheetseeheight);
+            console.log(sheetseehoriz);
+            console.log(sheetseehi);*/
+    var lineOptions = {
+    labels: sheetseelabel, 
+    units: sheetseeunit, 
+    m: sheetseemargin, 
+    w: sheetseewidth, 
+    h: sheetseeheight, 
+    div: '#'+sheetseeid, 
+    xaxis: sheetseehoriz,
+    hiColor: sheetseehi
+    };
+    Sheetsee.d3LineChart(data,lineOptions);
+    $("span.sheetseeline").empty();
+}    });
 } );
